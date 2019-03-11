ddbf457d3da0c2237e56f323682427b20cdd2ca3
Luke Badgerow <badgerow.luke@gmail.com>
2019-03-11 10:04:00 -0400

    moving things around to include all the demo work

---
+++ clustering-demo/IrisData.cs
@@ -0,0 +1,37 @@
+using Microsoft.ML.Data;
+namespace clustering_demo
+{
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float SepalLength;
+
+        [LoadColumn(1)]
+        public float SepalWidth;
+
+        [LoadColumn(2)]
+        public float PetalLength;
+
+        [LoadColumn(3)]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+    static class TestIrisData
+    {
+        internal static readonly IrisData Setosa = new IrisData{
+            SepalLength = 5.1f,
+            SepalWidth = 3.5f,
+            PetalLength = 1.4f,
+            PetalWidth = 0.2f
+        };
+    }
+
+}
---
+++ clustering-demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.Data.DataView;
+
+namespace clustering_demo
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+        
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed:0);
+            //set up data load
+            TextLoader textLoader = mlContext.Data.CreateTextLoader<IrisData>(false,',');
+            var dataView = textLoader.Read(_dataPath);
+
+            //create learning pipeline
+            string featuresColumnName = "Features";
+            var pipeline = mlContext.Transforms
+                                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, clustersCount: 3));
+
+            
+            //train the model
+            var model  = pipeline.Fit(dataView);
+
+            //save the model
+            using(var fs = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, fs);
+            }
+
+            //make predictions
+            var predictor = model.CreatePredictionEngine<IrisData, ClusterPrediction>(mlContext);
+            var prediction= predictor.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
